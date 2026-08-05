4ebc3d8731d5dac857fbc72d9c9d70212e84b5be
Callum Hopkins <callum@hopkins.family>
2026-08-05 07:34:21 +0100

    nixos: update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/1559d3daa3ecc813a650b79375ea61b6741b8746";
+    nixpkgs.url = "github:NixOS/nixpkgs/e72e4f299401a3689d4b3d5fc6496b11db7064eb";
 
     home-manager = {
       url = "github:nix-community/home-manager";
